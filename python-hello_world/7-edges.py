c43bb8c630da16d3d591fc06f341db7bb52528c7
Abdullatif00-A <A.alzaher0@hotmail.com>
2025-05-18 08:44:52 +0000

    7-edges.py

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 word = "olberto"
-print(f"word_firse_3 {word[0:3]}")
-print(f"word_last_2 {word[-2:]}")
-print(f"Middle word {word}")
+wored = "Holberton"
+print(f"word_firse_3 {wored[0:3]}")
+print(f"word_last_2 {wored[-2:]}")
+print(f"Middle word: {word}")
