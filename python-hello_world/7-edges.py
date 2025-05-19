063fd5256d14e7d3174fc5afa7d2c5398804445a
Abdullatif00-A <A.alzaher0@hotmail.com>
2025-05-19 03:06:46 +0000

    7-edges.py

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-wored = "Holberton"
-print(f"First 3 letters: {wored[0:3]}")
-print(f"Last 2 letters: {wored[-2:]}")
-print(f"Middle word: {wored [1:8]}")
+word = "Holberton"
+print(f"First 3 letters: {word[0:3]}")
+print(f"Last 2 letters: {word[-2:]}")
+print(f"Middle word: {word[1:-1]}")
