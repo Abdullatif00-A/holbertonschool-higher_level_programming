8ee4a97818a1c1a3b2c66320d256c09a7a4714be
Abdullatif00-A <A.alzaher0@hotmail.com>
2025-05-19 04:36:18 +0000

    8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str1 = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(f"{str1}")
+print(f"{str1[39:66]}{str1[106:111]} {str1[0:6]}")
